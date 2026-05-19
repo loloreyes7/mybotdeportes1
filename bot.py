163ec9ed652deffd0e967a4536d900fb099383cd
loloreyes7 <112188738+loloreyes7@users.noreply.github.com>
2026-05-19 23:59:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,26 @@
 name: Ejecutar Bot de Deportes
 on:
-schedule:
-- cron: '*/30 * * * *' # Se ejecuta cada 30 minutos
-workflow_dispatch: # Permite ejecutarlo manualmente desde GitHub
+  schedule:
+    - cron: '*/30 * * * *'
+  workflow_dispatch:
 
 jobs:
-build:
-runs-on: ubuntu-latest
-steps:
-- name: Checkout repo
-uses: actions/checkout@v3
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout repo
+        uses: actions/checkout@v3
 
-  - name: Setup Python
-    uses: actions/setup-python@v4
-    with:
-      python-version: '3.9'
+      - name: Setup Python
+        uses: actions/setup-python@v4
+        with:
+          python-version: '3.9'
 
-  - name: Instalar dependencias
-    run: pip install -r requirements.txt
+      - name: Instalar dependencias
+        run: pip install -r requirements.txt
 
-  - name: Ejecutar Bot
-    env:
-      TELEGRAM_TOKEN: ${{ secrets.TELEGRAM_TOKEN }}
-      CHAT_ID: ${{ secrets.CHAT_ID }}
-    run: python bot_deportes.py
-
-
-
-        
+      - name: Ejecutar Bot
+        env:
+          TELEGRAM_TOKEN: ${{ secrets.TELEGRAM_TOKEN }}
+          CHAT_ID: ${{ secrets.CHAT_ID }}
+        run: python bot_deportes.py
